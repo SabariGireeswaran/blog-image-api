f08d4cbff455bd707ef3172d726008120d591a1f
Sabari Gireeswaran B S R <bsrsabarigireeswaran@gmail.com>
2026-02-11 22:08:21 +0530

    feat: add GET Post API and status image serving - mounted uploads folder as static files - implemented GET /posts - implemented GET /posts/{id} - return image URLs for frontend usage - improved post creation response

--- main.py
+++ main.py
@@ -7,9 +7,14 @@ import os
 from database import SessionLocal, engine, Base
 from models import Post
 
+from fastapi.staticfiles import StaticFiles
+
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+os.makedirs("uploads", exist_ok=True)
+app.mount("/uploads", StaticFiles(directory="uploads"), name="uploads")
 
 UPLOAD_DIR = "uploads"
 os.makedirs(UPLOAD_DIR, exist_ok=True)
@@ -24,41 +29,65 @@ def get_db():
 
 #--------- CREATE POST ---------------
 @app.post("/posts")
-def create_post(
+async def create_post(
     title: str = Form(...),
     content: str = Form(...),
     image: UploadFile = File(None),
     db: Session = Depends(get_db)
 ):
-    try:
-        image_path = None
-        if image:
-            os.makedirs("uploads", exist_ok=True)
-            file_path=f"uploads/{image.filename}"
+    image_path = None
 
-            with open(file_path, "wb") as buffer:
-                shutil.copyfileobj(image.file, buffer)
+    if image:
+        file_path=f"uploads/{image.filename}"
+        with open(file_path, "wb") as buffer:
+            buffer.write(await image.read())
+        image_path = file_path
+    
+    new_post = Post(
+        title=title,
+        content=content,
+        image_path=image_path
+    )
 
-            image_path = file_path
+    db.add(new_post)
+    db.commit()
+    db.refresh(new_post)
 
-            new_post = Post(
-                title=title,
-                content=content,
-                image_path=image_path
-            )
-            db.add(new_post)
-            db.commit()
-            db.refresh(new_post)
-
-            return {
-                "id": new_post.id,
-                "title": new_post.title,
-                "content": new_post.content,
-                "image_path": new_post.image_path
-            }
-    except Exception as e:
-        return {"error": str(e)}
+    return {
+        "id": new_post.id,
+        "title": new_post.title,
+        "content": new_post.content,
+        "image_url": f"/{image_path}" if image_path else None
+    }
 
 @app.get("/")
 def home():
     return {"message": "Blog API  running"}
+
+@app.get("/posts")
+def get_posts(db: Session = Depends(get_db)):
+    posts = db.query(Post).all()
+
+    result = []
+    for p in posts:
+        result.append({
+            "id": p.id,
+            "title": p.title,
+            "content": p.content,
+            "image_url": f"/{p.image_path}" if p.image_path else None
+        })
+    return result
+
+@app.get("/posts/{post_id}")
+def get_post(post_id: int, db: Session = Depends(get_db)):
+    post = db.query(Post).filter(Post.id == post_id).first()
+
+    if not post:
+        raise HTTPException(404, "Post not found")
+    
+    return {
+        "id": post.id,
+        "title": post.title,
+        "content": post.content,
+        "image_url": f"http://127.0.0.1:8000/{post.image_path}" if post.image_path else None
+    }
